cdcdb2cd92f1ebd9e34d1c215134c4c33053d06a
Jeya <shreejeya066@gmail.com>
2026-03-29 13:20:51 +0530

    Update API_BASE_URL to point to the correct Vercel backend link

---
+++ check_data.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Student = require('./modules/studentModel');
+require('dotenv').config();
+
+const test = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        const students = await Student.find({}).limit(10);
+        console.log('Students in DB:', JSON.stringify(students, null, 2));
+        process.exit(0);
+    } catch (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+};
+
+test();
